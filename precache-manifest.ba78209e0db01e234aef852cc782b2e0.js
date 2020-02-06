ba659b7ac9a93e47a837920c658daf1dde3c3048
ibulavsky <ibulavsky@gmail.com>
2020-02-06 16:25:40 +0300

    Updates

---
+++ precache-manifest.ba78209e0db01e234aef852cc782b2e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6da441b051568fdb3a96cf9126195d8",
+    "url": "/to-do-lists-online/index.html"
+  },
+  {
+    "revision": "2887050499c250b8480f",
+    "url": "/to-do-lists-online/static/css/2.f16bc208.chunk.css"
+  },
+  {
+    "revision": "c8344c5a49abd47a8c92",
+    "url": "/to-do-lists-online/static/css/main.e729181e.chunk.css"
+  },
+  {
+    "revision": "2887050499c250b8480f",
+    "url": "/to-do-lists-online/static/js/2.54549e93.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/to-do-lists-online/static/js/2.54549e93.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c8344c5a49abd47a8c92",
+    "url": "/to-do-lists-online/static/js/main.df49482e.chunk.js"
+  },
+  {
+    "revision": "bb1012050fdc876a31b2",
+    "url": "/to-do-lists-online/static/js/runtime-main.a5b02ea5.js"
+  }
+]);
